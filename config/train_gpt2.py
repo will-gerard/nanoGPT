417777edb5ad34ba5b5edb57f93f360c1aacd8d4
William Gerard <whgerard1@gmail.com>
2023-05-06 06:07:48 +0000

    Modify transfer network and forward pass to make dimensions match
    After this change, each batch results in a single scalar output from the network,
    to guarantee accurate broadcasting it is done manually. The x tensor, which started
    out as (batch size x block_size x n_embd) is flattened along the second and third
    dimensions, and then the output from the transfer sequential block  is squeezed down
    to just the batch dimension before it is used to compute the loss.
    
    We are now able to use a larger batch size, because of the padding changes added earlier.
    However, overall this change slows down the training job a lot.

--- config/train_gpt2.py
+++ config/train_gpt2.py
@@ -3,13 +3,13 @@
 # $ torchrun --standalone --nproc_per_node=8 train.py config/train_gpt2.py
 
 wandb_log = True
-wandb_project = 'owt'
-wandb_run_name='gpt2-124M'
+wandb_project = 'NanoGPT-TransferLearning'
+wandb_run_name='transfer-test'
 
 # these make the total batch size be ~0.5M
 # 12 batch size * 1024 block size * 5 gradaccum * 8 GPUs = 491,520
-batch_size = 12
-block_size = 1024
+batch_size = 32 # collapsing down to just a 1D vector for loss calculation
+block_size = 256
 gradient_accumulation_steps = 5 * 8
 
 # this makes total number of tokens be 300B
@@ -17,8 +17,8 @@ max_iters = 600000
 lr_decay_iters = 600000
 
 # eval stuff
-eval_interval = 1000
-eval_iters = 200
+eval_interval = 30
+eval_iters = 20
 log_interval = 10
 
 # weight decay
