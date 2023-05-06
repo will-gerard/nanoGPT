6ecadc8baf08d40d09994b2639be6f06e9ca6fcf
William Gerard <whgerard1@gmail.com>
2023-05-06 16:09:23 +0000

    Update config to latest parameters

--- config/train_gpt2.py
+++ config/train_gpt2.py
@@ -1,14 +1,14 @@
 # config for training GPT-2 (124M) down to very nice loss of ~2.85 on 1 node of 8X A100 40GB
 # launch as the following (e.g. in a screen session) and wait ~5 days:
 # $ torchrun --standalone --nproc_per_node=8 train.py config/train_gpt2.py
-
+import time
 wandb_log = True
 wandb_project = 'NanoGPT-TransferLearning'
-wandb_run_name='transfer-test'
+wandb_run_name='transfer-test' + str(time.time())
 
 # these make the total batch size be ~0.5M
 # 12 batch size * 1024 block size * 5 gradaccum * 8 GPUs = 491,520
-batch_size = 32 # collapsing down to just a 1D vector for loss calculation
+batch_size = 8
 block_size = 256
 gradient_accumulation_steps = 5 * 8
 
@@ -18,7 +18,7 @@ lr_decay_iters = 600000
 
 # eval stuff
 eval_interval = 30
-eval_iters = 20
+eval_iters = 5
 log_interval = 10
 
 # weight decay
